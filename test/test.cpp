a9feaa3f989c9ed900193726cb034074c3df9d8a
danielforever <daniel87011354@gmail.com>
2022-10-15 11:33:58 +0800

    add LoadVideo test case.

--- test/test.cpp
+++ test/test.cpp
@@ -3,8 +3,14 @@
  *
  */
 #include <gtest/gtest.h>
+#include <./perception.h>
 
+Camera video1;
 
-TEST(, should_pass) {
-  EXPECT_EQ(1, 1);
+TEST(LoadVideo, case1) {
+  EXPECT_EQ(video1.LoadVideo(./video/sample1), 1);
+}
+
+TEST(LoadVideo, case2) {
+  EXPECT_EQ(video1.LoadVideo(), -1);
 }
