b73d72eb81e8609683742ca07c90f0bbea9cd377
danielforever <daniel87011354@gmail.com>
2022-10-20 04:16:04 +0800

    Task 1.12: Creating Unit test for Human Tracker Class

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,17 @@
+/**
+ * @file test.cpp
+ * @Author(s) Po-Yu Huang, Aman Sharma, Shantanu
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-19
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <gtest/gtest.h>
+#include "./perception.h"
 
 
-TEST(, should_pass) {
+TEST(Human_Tracker Constructor, should_pass) {
   EXPECT_EQ(1, 1);
 }
