106d4404f21b22fd2edd466052e7194915399a0c
danielforever <daniel87011354@gmail.com>
2022-10-28 15:56:22 +0800

    Task 3.15: Modify the CleanAndDisplayfunction function test case

--- app/main.cpp
+++ app/main.cpp
@@ -20,12 +20,15 @@
 int main() {
   Human_Tracker a;
   Human_Tracker b;
+  Human_Tracker c;
   a.SetVideoDirectory("../assets/videos/single_person.mp4");
   b.SetVideoDirectory("../assets/images/pedestrian_single.jpg");
+  c.SetVideoDirectory("../assets/videos/double_simple.mp4");
   Camera h;
   //h.LoadImage(a.GetVideoDirectory());
-  Detector d(h.LoadVideo(a.GetVideoDirectory(), "video"), h.videoorimage);
+  //Detector d(h.LoadVideo(a.GetVideoDirectory(), "video"), h.videoorimage);
   //Detector d(h.LoadVideo(b.GetVideoDirectory(), "image"), h.videoorimage);
+  Detector d(h.LoadVideo(c.GetVideoDirectory(), "video"), h.videoorimage);
   Tracker t;
   Mat frame;
   d.getOutputsNames();
